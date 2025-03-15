398506e9c83fda1e05a33255e4736a0864eb4fc9
Abderrahmane Mihoubi <mihoubiabderrahmane09@gmail.com>
2025-03-15 05:33:50 +0100

    Initial Commit

---
+++ src/test/java/com/victorlicht/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlicht.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
